929b208cbd3dd45c7cb1f211c9e0823fb6d07b9b
An Vu <hoanganwm@gmail.com>
2023-10-08 15:30:53 +0700

    clean-calculateDayInMonth-function

--- src/main/java/com/anvu/datetimechecker/resource/DateTimeChecker.java
+++ src/main/java/com/anvu/datetimechecker/resource/DateTimeChecker.java
@@ -123,11 +123,7 @@ public class DateTimeChecker {
                 return 30;
             }
             case 2: {
-                if (year%400 ==0) {
-                    return 29;
-                } else if (year%100 == 0) {
-                    return 28;
-                } else if (year%4 == 0) {
+                if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
                     return 29;
                 } else {
                     return 28;
