f9e2f59b35187ba5af3eb6e3ae0786c73a0760db
An Vu <hoanganwm@gmail.com>
2023-10-17 13:53:26 +0700

    fix-bug-test

--- src/main/java/com/anvu/datetimechecker/resource/DateTimeChecker.java
+++ src/main/java/com/anvu/datetimechecker/resource/DateTimeChecker.java
@@ -114,7 +114,7 @@ public class DateTimeChecker {
                 }
             }
         } else {
-            checkErrBoxContainKeyDay = false;
+            checkErrBoxContainKeyDay = true;
         }
         if (!errBox.getDescription().isEmpty()) {
             if (errBox.getDescription().containsKey("day") && !errBox.getCodes().contains(ErrorCode.INVALID_DATA) && !checkErrBoxContainKeyDay) {
